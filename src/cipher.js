f84aa05f6897e34e1cc4739eaa17f8eb3932aeb6
Angelica Bolivar <angelica.bolivarvc@gmail.com>
2022-02-01 23:05:09 -0500

    ADD - cipher : decode function

--- src/cipher.js
+++ src/cipher.js
@@ -13,13 +13,13 @@ const cipher = {
     },
     decode: (offset, string) => {
         let messg = ""
-            // for (let i = 0; i < string.length; i++) {
-            //     let posAscii = string.charCodeAt(i);
-            //     let posOffset = (posAscii - 65 + offset) % 26 + 65;
-            //     let letra = String.fromCharCode(posOffset);
-            //     messg = messg + letra;
+        for (let i = 0; i < string.length; i++) {
+            let posAscii = string.charCodeAt(i);
+            let posOffset = (posAscii + 65 - offset) % 26 + 65;
+            let letra = String.fromCharCode(posOffset);
+            messg = messg + letra;
 
-        // }
+        }
         return messg
     }
 };
